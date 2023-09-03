3859f85bd5a8930c17be195f53444873e064266a
Wy <111397148+wyrla@users.noreply.github.com>
2023-09-02 22:16:32 -0300

    test: ensure SignUpCtlr return an error when email, password and confirmPassword aren't provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,46 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
+
+  it('should return 400 if email was not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'some_name',
+        password: 'some_password',
+        confirmPassword: 'some_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+
+  it('should return 400 if password was not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'some_name',
+        email: 'some_email@provider',
+        confirmPassword: 'some_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: password'))
+  })
+
+  it('should return 400 if confirmPassword was not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'some_name',
+        password: 'some_password',
+        email: 'some_email@provider'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: confirmPassword'))
+  })
 })
