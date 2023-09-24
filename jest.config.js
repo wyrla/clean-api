0ca70da2f6cabcc615f22b92e31f1b32c8bd751a
wyrla <111397148+wyrla@users.noreply.github.com>
2023-09-24 12:59:49 -0300

    chore: update jest collectCoverageFrom to ignore files that only aggregate exports

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   roots: [
     '<rootDir>/src'
