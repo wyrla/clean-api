a69d0e2440296459ad95e76dffd863d66bbb9ba7
wyrla <111397148+wyrla@users.noreply.github.com>
2023-09-24 13:00:04 -0300

    feat: ensure AccountMongoRepository return an account on success

--- src/infra/cryptography/db/mongodb/account-repository/account.ts
+++ src/infra/cryptography/db/mongodb/account-repository/account.ts
@@ -1,9 +1,25 @@
+import { Collection } from "mongodb";
 import { AddAccountRepository } from "../../../../../data/protocols/add-account-repository";
 import { AccountModel } from "../../../../../domain/models/account";
 import { AddAccountModel } from "../../../../../domain/usecases/add-account";
+import { mongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-    add(account: AddAccountModel): Promise<AccountModel> {
-        return new Promise((resolve) => resolve({ email: account.email, id: '1', name: account.name, password: account.password}))
+    private accountCollection = mongoHelper.getCollection('accounts')
+
+    async add(accountData: AddAccountModel): Promise<AccountModel> {
+        const result = await this.accountCollection.insertOne({
+            email: accountData.email,
+            name: accountData.name,
+            password: accountData.password
+        })
+        
+        const { _id, ...account } = await this.accountCollection.findOne({_id: result.insertedId})
+
+        return { 
+            id: String(_id),
+            ...account
+        } as AccountModel
     }
+  
 }
