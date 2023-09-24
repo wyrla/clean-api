ba24a96ab06e3fab7d0986f565768001e3c09358
wyrla <111397148+wyrla@users.noreply.github.com>
2023-09-24 13:00:03 -0300

    test: ensure AccountMongoRepository return an account on success

--- src/infra/cryptography/db/mongodb/account-repository/account.spec.ts
+++ src/infra/cryptography/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from "../helpers/mongo-helper"
+import { AccountMongoRepository } from "./account"
 
 describe('Account Mongo Repository', () => {
     const mongoHelper = new MongoHelper()
@@ -10,5 +11,19 @@ describe('Account Mongo Repository', () => {
 
     afterAll(async () => {
         await mongoHelper.disconnect()
+    })
+
+    test('should return an account on success', async () => {
+        const sut = new AccountMongoRepository()
+        const account = await sut.add({
+            name: 'any_name',
+            email: 'any_email@provider.com',
+            password: 'any_password'
+        })
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email@provider.com')
+        expect(account.password).toBe('any_password')
     })
 })
