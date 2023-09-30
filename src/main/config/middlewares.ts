bcdf39ebb37d299515b49bce8da954dc01c726b5
wyrla <111397148+wyrla@users.noreply.github.com>
2023-09-29 21:17:27 -0300

    refactor: group middlewares interfaces in a sigle file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,8 @@
 import { type Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { cors } from '../middlewares/cors'
-import { contentType } from '../middlewares/content-type'
+import { bodyParser, contentType, cors } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
-  app.use(cors)
   app.use(contentType)
+  app.use(cors)
 }
