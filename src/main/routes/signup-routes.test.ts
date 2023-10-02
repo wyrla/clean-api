954d6d838d19a647e9579911d47ab9b4ca4c079a
wyrla <111397148+wyrla@users.noreply.github.com>
2023-10-01 21:45:58 -0300

    test: connect to mongodb before integrations test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { mongoHelper } from '../../infra/cryptography/db/mongodb/helpers/mongo-helper'
 
 describe('Sign Up Routes', () => {
+  const mongoDbHelper = mongoHelper
+
+  beforeAll(async () => {
+    await mongoDbHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.client.db().collection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await mongoDbHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
