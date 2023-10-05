ca916078d77bb902d12b8a77ed09f0a1683fc75b
wyrla <111397148+wyrla@users.noreply.github.com>
2023-10-04 21:55:23 -0300

    chore: remove console log

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,6 @@
 import { mongoHelper } from '../infra/cryptography/db/mongodb/helpers/mongo-helper'
 import app from './config/app'
 import env from './config/env'
-console.log(env.mongoUrl)
 
 mongoHelper.connect(env.mongoUrl).then(() => {
   app.listen(env.port, () => { console.log(`server running at: http://localhost:${env.port}`) })
