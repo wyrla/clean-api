4d123a7a790f97c0da341710a4b794e2d586165d
wyrla <111397148+wyrla@users.noreply.github.com>
2023-10-12 11:32:59 -0300

    feat: create signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BCryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/cryptography/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const emailValidator = new EmailValidatorAdapter()
+  const salt = 12
+  const bcryptAdapter = new BCryptAdapter(salt)
+  const accountRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountRepository)
+  return new SignUpController(emailValidator, dbAddAccount)
+}
