5e00f61e87673e84fb80de140f6876aece0d19cd
wyrla <111397148+wyrla@users.noreply.github.com>
2023-10-12 19:37:06 -0300

    feat: ensure mongo heleper reconnect if db is disconnected

--- src/infra/cryptography/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/cryptography/db/mongodb/helpers/mongo-helper.ts
@@ -2,20 +2,22 @@ import { type Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
   async disconnect (): Promise<void> {
     await this.client.close()
   },
   async getCollection (name: string): Promise<Collection> {
+    if (!this.client) await this.connect(this.uri)
     return this.client.db().collection(name)
   },
   map (document: any): any {
     const { _id, ...data } = document
     return { id: String(_id), ...data }
   }
-
 }
 
 // export class MongoHelper {
