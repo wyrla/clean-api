e2f2a56cb40d3e60a62e2b792ac238743a69ee84
Wy <111397148+wyrla@users.noreply.github.com>
2023-10-14 20:59:25 -0300

    feat: ensure LogControllerDecorator calls controller with correct input

---
+++ src/infra/decorators/log-controller-decorator.ts
@@ -0,0 +1,10 @@
+import type { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  constructor (private readonly controller: Controller) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpRequest)
+    return null
+  }
+}
