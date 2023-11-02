71b68297103aa0db17f49dc3ef38b855cf17d76e
wyrla <111397148+wyrla@users.noreply.github.com>
2023-11-02 18:40:59 -0300

    refactor: move required field validation to the validation composite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,17 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { type Validation } from '../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { makeSignUpValidation } from './signup-validation'
+
+jest.mock('../../presentation/helpers/validators/validation-composite')
+
+describe('SignUp Validation factory', () => {
+  test('Should call validation composite with all validations', async () => {
+    makeSignUpValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'confirmPassword']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,5 +1,11 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { type Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 
 export const makeSignUpValidation = (): ValidationComposite => {
-  return new ValidationComposite([])
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'confirmPassword']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
 }
