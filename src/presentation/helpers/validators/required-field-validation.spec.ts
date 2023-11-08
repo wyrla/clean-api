e28b4bc14badab8ebb82924352b51864ad1ca42f
wyrla <111397148+wyrla@users.noreply.github.com>
2023-11-08 12:00:06 -0300

    test: ensure RequiredValidation not return an error validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Field Validation', () => {
     const error = sut.validate({ password: 'any_password' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should not return if validation succeed', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
